07247778d435e8fd1873038ac6fb0c9c41fda20f
Yusuf Ismail <linkinpark_neon@yahoo.com>
2025-01-15 19:53:34 +0800

    💬 change text to support multiple languages

---
+++ data.js
@@ -0,0 +1,13 @@
+const data = [
+    {
+        word: "hello",
+        ipa_pronunciation: ["həˈloʊ", "hɛˈloʊ"],
+        translations: {
+            vn: ["xin chào"],
+            my: ["hai"], 
+            zh: ["你好"],
+            ja: ["こんにちは"],
+            ar: ["مرحبا"]
+        },
+    }
+];
